dbc7964644823ed09ea5b8c8eaec1abc65f4a627
bernardho95 <bernardocordeiro100@hotmail.com>
2020-03-18 14:56:30 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-          title: "Aula 3",
-          theme: ThemeData(
-          primaryColor: Colors.blue
-          ),
-          home: Meuwidget()
-          
-        );
-        
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-class Meuwidget extends StatelessWidget {
-  
-
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-     color: Colors.orange
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Aula 3",
+        theme: ThemeData(primaryColor: Colors.blue),
+        home: meuWidget());
+  }
+
+  meuWidget() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Projeto Parque"),
+        centerTitle: true,
+      ),
+      body: Container(color: Colors.white),
+      drawer: Container(
+        color: Colors.green,
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_a_photo),
+          onPressed: () {
+            print("Pressionado");
+          }),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Local'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categoria'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Pesquisar'),
+          ),
+        ],
+      ),
     );
   }
 }
